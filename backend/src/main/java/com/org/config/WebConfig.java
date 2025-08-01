c0ea5461aa8c3ad058a8eea044d265a4f0acc057
Anurag Singh <90509642+scanurag@users.noreply.github.com>
2025-08-01 12:31:31 +0530

    Update WebConfig.java

--- backend/src/main/java/com/org/config/WebConfig.java
+++ backend/src/main/java/com/org/config/WebConfig.java
@@ -1,5 +1,4 @@
-package com.yourpackage.config; 
-
+package com.org.config; 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -7,16 +6,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig {
-
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/api/**")
+                registry.addMapping("/**")
                         .allowedOrigins("https://jobify-1-i2eu.onrender.com")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE")
-                        .allowedHeaders("*");
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
             }
         };
     }
