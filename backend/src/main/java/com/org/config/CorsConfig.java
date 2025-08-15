00a85f98e6045cd1f5a7aff5da0d5850d4ebf287
scanurag <scanurag@gmail.com>
2025-08-15 22:47:35 +0530

    Update backend build and Docker image details

--- backend/src/main/java/com/org/config/CorsConfig.java
+++ backend/src/main/java/com/org/config/CorsConfig.java
@@ -1,28 +1,30 @@
 package com.org.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 public class CorsConfig {
-
     @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/api/**")
-                        .allowedOrigins(
-                                "http://localhost:3000",
-                                "https://jobify-1-3ow4.onrender.com"
-                                
-                        )
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*")
-                        .exposedHeaders("Authorization", "Content-Type")
-                        .allowCredentials(true);
-            }
-        };
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        
+        config.setAllowCredentials(true);
+        
+        // Allowed Origins
+        config.addAllowedOrigin("https://jobify-1-3ow4.onrender.com");
+        config.addAllowedOrigin("http://localhost:3000");
+        
+        // Allowed Headers & Methods
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        
+        // Apply CORS config to all endpoints
+        source.registerCorsConfiguration("/**", config);
+        
+        return new CorsFilter(source);
     }
 }
